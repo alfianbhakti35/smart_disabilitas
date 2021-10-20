aa543330356a861202e02b08e9b33fc0475a2526
berkahIT <berkahit1178@gmail.com>
2021-10-21 01:18:50 +0800

    perbaikan bug

--- app/Http/Controllers/FakultasController.php
+++ app/Http/Controllers/FakultasController.php
@@ -2,36 +2,18 @@
 
 namespace App\Http\Controllers;
 
-<<<<<<< HEAD
-=======
 use App\Models\Fakultas;
->>>>>>> f5d949da1fcdf9803d83e7f094055fbe9baf94e6
 use Illuminate\Http\Request;
 
 class FakultasController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
-<<<<<<< HEAD
         return view('backend/fakultas/index',[
             "title" => "Fakultas"
         ]);
-=======
-        //
->>>>>>> f5d949da1fcdf9803d83e7f094055fbe9baf94e6
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-<<<<<<< HEAD
 
     public function tambah()
     {
@@ -39,99 +21,12 @@ class FakultasController extends Controller
             "title" => "Fakultas"
         ]);
     }
-
-=======
->>>>>>> f5d949da1fcdf9803d83e7f094055fbe9baf94e6
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-<<<<<<< HEAD
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-=======
-     * @param  \App\Models\Fakultas  $fakultas
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Fakultas $fakultas)
->>>>>>> f5d949da1fcdf9803d83e7f094055fbe9baf94e6
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-<<<<<<< HEAD
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function edit()
     {
         return view('backend/fakultas/edit',[
             "title" => "Fakultas"
         ]);
-=======
-     * @param  \App\Models\Fakultas  $fakultas
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Fakultas $fakultas)
-    {
-        //
->>>>>>> f5d949da1fcdf9803d83e7f094055fbe9baf94e6
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-<<<<<<< HEAD
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-=======
-     * @param  \App\Models\Fakultas  $fakultas
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Fakultas $fakultas)
->>>>>>> f5d949da1fcdf9803d83e7f094055fbe9baf94e6
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-<<<<<<< HEAD
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-=======
-     * @param  \App\Models\Fakultas  $fakultas
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Fakultas $fakultas)
->>>>>>> f5d949da1fcdf9803d83e7f094055fbe9baf94e6
-    {
-        //
-    }
 }
